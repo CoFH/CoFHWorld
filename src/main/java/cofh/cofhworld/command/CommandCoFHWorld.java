1a7ecba5ca099dafcce31ee0eb5c67b5724c25ce
skyboy <mr.wraith.06@gmail.com>
2018-05-14 10:03:15 -0400

    Prettify and paginate `list` command
    and also don't send it to the entire server

--- src/main/java/cofh/cofhworld/command/CommandCoFHWorld.java
+++ src/main/java/cofh/cofhworld/command/CommandCoFHWorld.java
@@ -6,7 +6,12 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.server.command.CommandTreeBase;
+
+import java.util.List;
 
 public class CommandCoFHWorld extends CommandTreeBase {
 
@@ -72,12 +77,23 @@ public class CommandCoFHWorld extends CommandTreeBase {
 		@Override
 		public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 
+			final int pageSize = 8;
+			List<IFeatureGenerator> generators = WorldHandler.getFeatures();
+			int maxPages = (generators.size() - 1) / pageSize;
+			int page = args.length == 0 ? 0 : CommandBase.parseInt(args[0], 1, maxPages + 1) - 1;
+
+			TextComponentTranslation component = new TextComponentTranslation("cofhworld.list", page + 1, maxPages + 1);
+			component.getStyle().setColor(TextFormatting.GOLD);
+			sender.sendMessage(component);
+
 			StringBuilder b = new StringBuilder();
-			b.append("\n");
-			for (IFeatureGenerator feature : WorldHandler.getFeatures()) {
-				b.append("* " + feature.getFeatureName() + "\n");
+			int maxIndex = Math.min((page + 1) * pageSize, generators.size());
+			for (int i = page * pageSize; i < maxIndex; ++i) {
+				b.append("* ").append(generators.get(i).getFeatureName()).append('\n');
 			}
-			notifyCommandListener(sender, this, "cofhworld.list", b.toString());
+			b.deleteCharAt(b.length() - 1);
+
+			sender.sendMessage(new TextComponentString(b.toString()));
 		}
 	}
 
