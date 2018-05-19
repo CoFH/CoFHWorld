e0876fb5efb57cf57fbbc046fddfee973ed78e10
skyboy <mr.wraith.06@gmail.com>
2018-05-19 19:58:15 -0400

    Bump version for this branch to 1.3, add version command

--- src/main/java/cofh/cofhworld/CoFHWorld.java
+++ src/main/java/cofh/cofhworld/CoFHWorld.java
@@ -21,8 +21,8 @@ public class CoFHWorld {
 	public static final String MOD_ID = "cofhworld";
 	public static final String MOD_NAME = "CoFH World";
 
-	public static final String VERSION = "1.2.0";
-	public static final String VERSION_MAX = "1.3.0";
+	public static final String VERSION = "1.3.0";
+	public static final String VERSION_MAX = "1.4.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
 
@@ -58,7 +58,7 @@ public class CoFHWorld {
 	@EventHandler
 	public void loadComplete(FMLLoadCompleteEvent event) {
 
-		WorldHandler.reloadConfig();
+		WorldHandler.reloadConfig(false);
 		config.save();
 
 		log.info(MOD_NAME + ": Load Complete.");
