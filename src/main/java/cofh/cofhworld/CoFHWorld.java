6501b5055be00a5a94a72734baea007de41648c3
King Lemming <kinglemming@gmail.com>
2020-04-03 16:57:48 -0700

    Update to new version.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/cofhworld/CoFHWorld.java
+++ src/main/java/cofh/cofhworld/CoFHWorld.java
@@ -21,7 +21,7 @@ public class CoFHWorld {
 	public static final String MOD_ID = "cofhworld";
 	public static final String MOD_NAME = "CoFH World";
 
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.4.0";
 	public static final String VERSION_MAX = "2.0.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
