6eed55a9ca5f08787fdfc88cb447ff16bdde5ec0
skyboy <skyboy026@gmail.com>
2020-09-20 01:28:21 -0400

    WorldGenDecoration uses ICondition internally

--- src/main/java/cofh/cofhworld/world/generator/WorldGenDecoration.java
+++ src/main/java/cofh/cofhworld/world/generator/WorldGenDecoration.java
@@ -1,6 +1,9 @@
 package cofh.cofhworld.world.generator;
 
 import cofh.cofhworld.data.DataHolder;
+import cofh.cofhworld.data.condition.ConstantCondition;
+import cofh.cofhworld.data.condition.ICondition;
+import cofh.cofhworld.data.condition.world.WorldValueCondition;
 import cofh.cofhworld.data.numbers.ConstantProvider;
 import cofh.cofhworld.data.numbers.INumberProvider;
 import cofh.cofhworld.data.numbers.random.SkellamRandomProvider;
@@ -11,18 +14,15 @@ import net.minecraft.world.World;
 import java.util.List;
 import java.util.Random;
 
-/**
- * @deprecated TODO: replace all booleans with ICondition
- */
-@Deprecated
 public class WorldGenDecoration extends WorldGen {
+
+	private static final ICondition SEE_SKY = new WorldValueCondition("CAN_SEE_SKY"), CHECK_STAY = new WorldValueCondition("BLOCK_VALUE_CAN_PLACE");
 
 	private final List<WeightedBlock> cluster;
 	private final WeightedBlock[] genBlock;
 	private final WeightedBlock[] onBlock;
 	private final INumberProvider clusterSize;
-	private boolean seeSky = true;
-	private boolean checkStay = true;
+	private ICondition seeSky, checkStay;
 	private INumberProvider stackHeight;
 	private INumberProvider xVar;
 	private INumberProvider yVar;
@@ -37,9 +37,9 @@ public class WorldGenDecoration extends WorldGen {
 
 		cluster = blocks;
 		clusterSize = count;
-		genBlock = material == null ? null : material.toArray(new WeightedBlock[material.size()]);
-		onBlock = on == null ? null : on.toArray(new WeightedBlock[on.size()]);
-		this.setStackHeight(1);
+		genBlock = material == null ? null : material.toArray(new WeightedBlock[0]);
+		onBlock = on == null ? null : on.toArray(new WeightedBlock[0]);
+		this.setStackHeight(1).setSeeSky(true).setCheckStay(true);
 		this.setXVar(new SkellamRandomProvider(8));
 		this.setYVar(new SkellamRandomProvider(4));
 		this.setZVar(new SkellamRandomProvider(8));
@@ -57,7 +57,7 @@ public class WorldGenDecoration extends WorldGen {
 		final int clusterSize = this.clusterSize.intValue(world, rand, data);
 
 		boolean r = false;
-		for (int l = clusterSize; l-- > 0; ) {
+		for (int l = clusterSize, tries = 0; l-- > 0; ) {
 			int x = xStart + xVar.intValue(world, rand, data.setPosition(start));
 			int z = zStart + zVar.intValue(world, rand, data.setPosition(start.add(x - xStart, 0, 0)));
 			int y = yStart + yVar.intValue(world, rand, data.setPosition(start.add(x - xStart, 0, z - zStart)));
@@ -65,15 +65,19 @@ public class WorldGenDecoration extends WorldGen {
 
 			if (!world.isBlockLoaded(pos)) {
 				++l;
+				if (++tries > 256) {
+					break; // yeah, okay. provided values are somewhere not loaded, we're not just 'missing' at the edges
+				}
 				continue;
 			}
 
-			if ((!seeSky || world.canSeeSky(pos)) && canGenerateInBlock(world, x, y - 1, z, onBlock) && canGenerateInBlock(world, x, y, z, genBlock)) {
+			WeightedBlock block = selectBlock(rand, cluster);
+			if (seeSky.checkCondition(world, rand, data.setPosition(pos).setBlock(block)) &&
+					canGenerateInBlock(world, x, y - 1, z, onBlock) && canGenerateInBlock(world, x, y, z, genBlock)) {
 
-				WeightedBlock block = selectBlock(rand, cluster);
-				int stack = stackHeight.intValue(world, rand, data.setPosition(pos));
+				int stack = stackHeight.intValue(world, rand, data);
 				do {
-					if (!checkStay || (block.block.canPlaceBlockAt(world, pos))) {
+					if (checkStay.checkCondition(world, rand, data)) {
 						r |= setBlock(world, rand, pos, block);
 					} else {
 						break;
@@ -91,11 +95,21 @@ public class WorldGenDecoration extends WorldGen {
 
 	public WorldGenDecoration setSeeSky(boolean seeSky) {
 
+		return setSeeSky(seeSky ? SEE_SKY : ConstantCondition.TRUE);
+	}
+
+	public WorldGenDecoration setCheckStay(boolean checkStay) {
+
+		return setCheckStay(checkStay ? CHECK_STAY : ConstantCondition.TRUE);
+	}
+
+	public WorldGenDecoration setSeeSky(ICondition seeSky) {
+
 		this.seeSky = seeSky;
 		return this;
 	}
 
-	public WorldGenDecoration setCheckStay(boolean checkStay) {
+	public WorldGenDecoration setCheckStay(ICondition checkStay) {
 
 		this.checkStay = checkStay;
 		return this;
@@ -112,6 +126,7 @@ public class WorldGenDecoration extends WorldGen {
 		return this;
 	}
 
+	@Deprecated
 	public WorldGenDecoration setXVar(int xVar) {
 
 		return this.setXVar(new ConstantProvider(xVar));
@@ -123,6 +138,7 @@ public class WorldGenDecoration extends WorldGen {
 		return this;
 	}
 
+	@Deprecated
 	public WorldGenDecoration setYVar(int yVar) {
 
 		return this.setYVar(new ConstantProvider(yVar));
@@ -134,6 +150,7 @@ public class WorldGenDecoration extends WorldGen {
 		return this;
 	}
 
+	@Deprecated
 	public WorldGenDecoration setZVar(int zVar) {
 
 		return this.setZVar(new ConstantProvider(zVar));
