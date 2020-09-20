3aabc44259efa5ed4da13edba09d9e2bb8cfbd21
skyboy <skyboy026@gmail.com>
2020-09-20 01:27:12 -0400

    Add more world-value conditions

--- src/main/java/cofh/cofhworld/data/condition/world/WorldValueEnum.java
+++ src/main/java/cofh/cofhworld/data/condition/world/WorldValueEnum.java
@@ -1,6 +1,9 @@
 package cofh.cofhworld.data.condition.world;
 
 import cofh.cofhworld.data.DataHolder;
+import cofh.cofhworld.util.random.WeightedBlock;
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.BlockFlower;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
@@ -9,26 +12,40 @@ import java.util.Random;
 
 public enum WorldValueEnum {
 
-	IS_AIR {
+	BLOCK_BUSH_CAN_STAY {
 		@Override
 		public boolean getValue(World world, Random rand, DataHolder data) {
 
-			return world.isAirBlock(new BlockPos(data.getPosition()));
+			WeightedBlock block = data.getBlock();
+			if (block.block instanceof BlockBush) {
+				return ((BlockBush) block.block).canBlockStay(world, new BlockPos(data.getPosition()), block.getState());
+			}
+			return false;
 		}
 	},
-	IS_HIGH_HUMIDITY {
+	BLOCK_FLOWER_CAN_STAY {
 		@Override
 		public boolean getValue(World world, Random rand, DataHolder data) {
 
-			return world.isBlockinHighHumidity(new BlockPos(data.getPosition()));
+			WeightedBlock block = data.getBlock();
+			if (block.block instanceof BlockFlower) {
+				return ((BlockFlower) block.block).canBlockStay(world, new BlockPos(data.getPosition()), block.getState());
+			}
+			return false;
 		}
 	},
-	IS_SPAWN_CHUNK {
+	BLOCK_CAN_PLACE {
 		@Override
 		public boolean getValue(World world, Random rand, DataHolder data) {
 
-			Vec3i pos = data.getPosition();
-			return world.isSpawnChunk(pos.getX() >> 4, pos.getZ() >> 4);
+			return data.getBlock().block.canPlaceBlockAt(world, new BlockPos(data.getPosition()));
+		}
+	},
+	CAN_RESPAWN {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.provider.canRespawnHere();
 		}
 	},
 	CAN_SEE_SKY {
@@ -43,6 +60,56 @@ public enum WorldValueEnum {
 		public boolean getValue(World world, Random rand, DataHolder data) {
 
 			return world.canSnowAt(new BlockPos(data.getPosition()), false);
+		}
+	},
+	DOES_WATER_VAPORIZE {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.provider.doesWaterVaporize();
+		}
+	},
+	HAS_SKY_LIGHT {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.provider.hasSkyLight();
+		}
+	},
+	IS_AIR {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.isAirBlock(new BlockPos(data.getPosition()));
+		}
+	},
+	IS_HIGH_HUMIDITY {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.isBlockinHighHumidity(new BlockPos(data.getPosition()));
+		}
+	},
+	IS_NETHER {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.provider.isNether();
+		}
+	},
+	IS_SPAWN_CHUNK {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			Vec3i pos = data.getPosition();
+			return world.isSpawnChunk(pos.getX() >> 4, pos.getZ() >> 4);
+		}
+	},
+	IS_SURFACE_WORLD {
+		@Override
+		public boolean getValue(World world, Random rand, DataHolder data) {
+
+			return world.provider.isSurfaceWorld();
 		}
 	};
 
