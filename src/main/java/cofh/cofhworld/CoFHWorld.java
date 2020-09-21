056f4d78a5ba24a3832e622ca1a672d830de8a12
skyboy <skyboy026@gmail.com>
2020-09-21 06:52:42 -0400

    Process worldgen configs on server start instead of client start

--- src/main/java/cofh/cofhworld/CoFHWorld.java
+++ src/main/java/cofh/cofhworld/CoFHWorld.java
@@ -2,11 +2,11 @@ package cofh.cofhworld;
 
 import cofh.cofhworld.init.WorldHandler;
 import cofh.cofhworld.init.WorldProps;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,15 +17,7 @@ public class CoFHWorld {
 	public static final String MOD_ID = "cofhworld";
 	public static final String MOD_NAME = "CoFH World";
 
-	public static final String VERSION = "2.0.0b";
-	public static final String VERSION_MAX = "2.1.0";
-	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
-	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
-
-	public static final String DEPENDENCIES = "required-after:forge@[" + "32.2.36,32.3.0" + ");";
-
 	public static Logger log = LogManager.getLogger(MOD_NAME);
-	//public static Configuration config;
 
 	public CoFHWorld() {
 
@@ -34,7 +26,7 @@ public class CoFHWorld {
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
 		modEventBus.addListener(this::preInit);
-		modEventBus.addListener(this::loadComplete);
+		MinecraftForge.EVENT_BUS.addListener(this::onServerStarting);
 
 		WorldHandler.register();
 	}
@@ -44,25 +36,16 @@ public class CoFHWorld {
 
 		//WorldProps.configDir = event.getModConfigurationDirectory();
 
-		//config = new Configuration(new File(WorldProps.configDir, "/cofh/world/config.cfg"), VERSION, true);
-		//config.load();
-
 		WorldProps.preInit();
 
 		WorldHandler.initialize();
 	}
 
-	public void loadComplete(FMLLoadCompleteEvent event) {
-
-		WorldHandler.reloadConfig(false);
-		//config.save();
-
-		log.info(MOD_NAME + ": Load Complete.");
-	}
-
-	public void onServerStarting(FMLServerStartingEvent event) {
+	public void onServerStarting(FMLServerAboutToStartEvent event) {
 
 		//event.registerServerCommand(new CommandCoFHWorld());
+
+		WorldHandler.reloadConfig(true);
 	}
 
 }
