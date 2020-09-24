8fe25c6e647a10ceec5978fb843c25f33b241a2e
skyboy <skyboy026@gmail.com>
2020-09-24 08:10:49 -0400

    Add MaterialPropertyMaterial

---
+++ src/main/java/cofh/cofhworld/data/block/MaterialPropertyMaterial.java
@@ -0,0 +1,48 @@
+package cofh.cofhworld.data.block;
+
+import net.minecraft.block.BlockState;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class MaterialPropertyMaterial extends Material {
+
+	private final Stream<Property> materials;
+
+	public MaterialPropertyMaterial(String... properties) {
+
+		this.materials = Arrays.stream(properties).map(v -> Property.valueOf(v.toUpperCase(Locale.US)));
+	}
+
+	@Override
+	public boolean test(final BlockState blockState) {
+
+		final net.minecraft.block.material.Material material = blockState.getMaterial();
+
+		return materials.allMatch(prop -> prop.test(material));
+	}
+
+	public static enum Property {
+		BLOCKS_MOVEMENT(net.minecraft.block.material.Material::blocksMovement),
+		FLAMMABLE(net.minecraft.block.material.Material::isFlammable),
+		REQUIRES_NO_TOOL(net.minecraft.block.material.Material::isToolNotRequired),
+		LIQUID(net.minecraft.block.material.Material::isLiquid),
+		OPAQUE(net.minecraft.block.material.Material::isOpaque),
+		REPLACEABLE(net.minecraft.block.material.Material::isReplaceable),
+		SOLID(net.minecraft.block.material.Material::isSolid);
+
+		private Property(Predicate<net.minecraft.block.material.Material> func) {
+
+			test = func;
+		}
+
+		public final Predicate<net.minecraft.block.material.Material> test;
+
+		final public boolean test(net.minecraft.block.material.Material material) {
+
+			return test.test(material);
+		}
+	}
+}
