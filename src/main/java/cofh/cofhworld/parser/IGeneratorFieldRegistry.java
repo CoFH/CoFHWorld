d19faa5dc12a21ebceeed43dbc971be5fcebc35f
skyboy <skyboy026@gmail.com>
2020-09-27 11:24:32 -0400

    Fix hanging raw generics

--- src/main/java/cofh/cofhworld/parser/IGeneratorFieldRegistry.java
+++ src/main/java/cofh/cofhworld/parser/IGeneratorFieldRegistry.java
@@ -8,9 +8,9 @@ import java.util.function.Supplier;
 
 public interface IGeneratorFieldRegistry<T extends IBuilder<? extends WorldGen>> {
 
-	IGeneratorFieldRegistry setBuilder(Supplier<T> builderSupplier);
+	IGeneratorFieldRegistry<T> setBuilder(Supplier<T> builderSupplier);
 
-	<V> IGeneratorFieldRegistry addRequiredField(String name, Type<V> type, BiConsumer<T, V> parser, String... keys);
+	<V> IGeneratorFieldRegistry<T> addRequiredField(String name, Type<V> type, BiConsumer<T, V> parser, String... keys);
 
-	<V> IGeneratorFieldRegistry addOptionalField(String name, Type<V> type, BiConsumer<T, V> parser, String... keys);
+	<V> IGeneratorFieldRegistry<T> addOptionalField(String name, Type<V> type, BiConsumer<T, V> parser, String... keys);
 }
