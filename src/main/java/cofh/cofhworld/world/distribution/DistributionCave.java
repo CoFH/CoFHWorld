cabce1c5b941db088c3bbf2ff7f8612764bb3a4d
skyboy <skyboy026@gmail.com>
2020-12-25 02:38:01 -0500

    Add floorLevel to `cave` distribution

--- src/main/java/cofh/cofhworld/world/distribution/DistributionCave.java
+++ src/main/java/cofh/cofhworld/world/distribution/DistributionCave.java
@@ -1,7 +1,11 @@
 package cofh.cofhworld.world.distribution;
 
 import cofh.cofhworld.data.DataHolder;
+import cofh.cofhworld.data.numbers.ConstantProvider;
 import cofh.cofhworld.data.numbers.INumberProvider;
+import cofh.cofhworld.data.numbers.data.DataProvider;
+import cofh.cofhworld.data.numbers.operation.MathProvider;
+import cofh.cofhworld.data.numbers.random.UniformRandomProvider;
 import cofh.cofhworld.data.numbers.world.WorldValueProvider;
 import cofh.cofhworld.world.generator.WorldGen;
 import net.minecraft.block.BlockState;
@@ -13,10 +17,14 @@ import java.util.Random;
 public class DistributionCave extends Distribution {
 
 	private final static INumberProvider GROUND_LEVEL = new WorldValueProvider("GROUND_LEVEL");
+	private final static INumberProvider INTERMEDIATE_LEVEL = new UniformRandomProvider(
+			ConstantProvider.ZERO,
+			new MathProvider(ConstantProvider.ONE, new DataProvider("ground-level"), "ADD")
+	);
 
 	private final WorldGen worldGen;
 	private final INumberProvider count;
-	private INumberProvider groundLevel = GROUND_LEVEL;
+	private INumberProvider groundLevel = GROUND_LEVEL, intermediateLevel = INTERMEDIATE_LEVEL, floorLevel = ConstantProvider.ZERO; // TODO: names
 	private final boolean ceiling;
 
 	public DistributionCave(String name, WorldGen worldGen, boolean ceiling, INumberProvider count, boolean regen) {
@@ -32,8 +40,13 @@ public class DistributionCave extends Distribution {
 		this.groundLevel = level;
 	}
 
+	public void setIntermediateLevel(INumberProvider level) {
+
+		this.intermediateLevel = level;
+	}
+
 	@Override
-	public boolean generateFeature(Random random, int blockX, int blockZ, ISeedReader world) {
+	public boolean generateFeature(Random random, int blockX, int blockZ, ISeedReader world) { // TODO: this logic needs looked at
 
 		BlockPos pos = new BlockPos(blockX, 64, blockZ);
 
@@ -57,23 +70,25 @@ public class DistributionCave extends Distribution {
 				seaLevel = world.getHeight();
 			}
 
-			int stopY = random.nextInt(1 + seaLevel / 2);
+			int stopY = intermediateLevel.intValue(world, random, data.setValue("ground-level", seaLevel)),
+					minY = floorLevel.intValue(world, random,data.setValue("intermediate-level", stopY));
 			int y = stopY;
 			BlockState state;
 			do {
 				state = world.getBlockState(new BlockPos(x, y, z));
 			} while (!state.getBlock().isAir(state, world, new BlockPos(x, y, z)) && ++y < seaLevel);
 
-			if (y == seaLevel) {
-				y = 0;
+			if (y == seaLevel & stopY > minY) {
+				y = minY;
 				do {
 					state = world.getBlockState(new BlockPos(x, y, z));
 				} while (!state.getBlock().isAir(state, world, new BlockPos(x, y, z)) && ++y < stopY);
-				if (y == stopY) {
+				if (y >= stopY) {
 					continue;
 				}
 			}
 
+			data.setValue("floor-level", minY);
 			if (ceiling) {
 				if (y < stopY) {
 					seaLevel = stopY + 1;
@@ -82,15 +97,15 @@ public class DistributionCave extends Distribution {
 					++y;
 					state = world.getBlockState(new BlockPos(x, y, z));
 				} while (y < seaLevel && state.getBlock().isAir(state, world, new BlockPos(x, y, z)));
-				if (y == seaLevel) {
+				if (y >= seaLevel) {
 					continue;
 				}
 			} else if (state.getBlock().isAir(state, world, new BlockPos(x, y - 1, z))) {
 				--y;
 				do {
 					state = world.getBlockState(new BlockPos(x, y, z));
-				} while (state.getBlock().isAir(state, world, new BlockPos(x, y, z)) && y-- > 0);
-				if (y == -1) {
+				} while (state.getBlock().isAir(state, world, new BlockPos(x, y, z)) && y-- > minY);
+				if (y <= minY) {
 					continue;
 				}
 			}
